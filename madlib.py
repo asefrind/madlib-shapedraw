2f9841cf950cd8e3bc3e56f6f2b392df1db5b908
asefrind <75707841+asefrind@users.noreply.github.com>
2021-02-17 12:09:28 -0300

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,15 @@
+# Madlib
+# Words for the sentence
+place = input("Choose a place: ")
+object1 = input("Choose an object: ")
+adjective = input("Choose an adjective: ")
+object2 = input("Choose another object: ")
+
+print(f"The other day i was at the {place}  when i looked at the {object1} and \
+    saw it was {adjective} so i ran to the sed, grabbed a {object2} and fixed it")
+
+
+
+
+
+    
